a9ceb1ad61a73a7abb3aab9794963a8353db132d
Afboy2506 <affiliatet73@gmail.com>
2024-07-14 23:22:01 +0700

    tindersex

---
+++ 69/bg.js
@@ -0,0 +1,19 @@
+$("#example, body").vegas({
+    delay: 3500,
+    timer: false,
+    shuffle: true,
+    timer: true,
+    transition: 'fade',
+    transitionDuration: 2500,
+    slides: [
+        {src: 'bg/1.jpg'},
+        {src: 'bg/2.jpg'},
+        {src: 'bg/3.jpg'},
+        {src: 'bg/4.jpg'},
+        {src: 'bg/6.jpg'},
+        {src: 'bg/7.jpg'},
+        {src: 'bg/9.jpg'},
+        {src: 'bg/10.jpg'}
+        
+    ]
+});
